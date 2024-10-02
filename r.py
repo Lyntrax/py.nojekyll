c89f121fda7bd45f2f46d0d9d72269a28297e1cd
Lyntrax <johncarlocaspe4@gmail.com>
2024-10-02 08:47:07 +0400

    git add

---
+++ r.py
@@ -0,0 +1,8 @@
+operator = input("Enter (/ * - +):")
+num1 = ("Enter 1st number:")
+num2 = ("Enter 2nd number:")
+if operator == "+":
+    resume = num1+num2
+    print (input(num1 + num2))
+elif operator == "-":
+    print(num1 - num2)
